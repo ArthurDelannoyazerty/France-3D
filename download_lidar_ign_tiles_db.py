0544771c15114350f0ac1b8c0b64d83ab8e0c633
ArthurDelannoyazerty <ah.delannoy@gmail.com>
2025-01-19 02:41:53 +0100

    laz to np array + 3d viz

--- download_lidar_ign_tiles_db.py
+++ download_lidar_ign_tiles_db.py
@@ -4,6 +4,10 @@ import logging
 import py7zr
 import os
 import zipfile
+import laspy
+import lazrs
+import numpy as np
+import pyvista as pv
 
 from utils.logger import setup_logging
 
@@ -33,6 +37,37 @@ def extract_zip(filepath:str, folderpath:str):
         archive.extractall(folderpath)
 
 
+def laz_to_numpy(filepath_in) -> np.ndarray:
+    with laspy.open(filepath_in, 'r') as las:
+        return np.array(las.read().points.array.tolist())       # Covnert numpy void to np array
+    # index 0  : X
+    # index 1  : Y
+    # index 2  : Z
+    # index 3  : ?
+    # index 4  : 
+    # index 5  : ?
+    # index 6  : ?
+    # index 7  : ?
+    # index 8  : ?
+    # index 9  : ?
+    # index 10 : ?
+
+
+def show_point_cloud(points, types):
+    """
+    Display a 3D point cloud using PyVista.
+    
+    Parameters:
+    - points (numpy.ndarray): A Nx3 array of 3D points (x, y, z).
+    """
+    point_cloud = pv.PolyData(points)
+    point_cloud["point_color"] = types
+    plotter = pv.Plotter()
+    plotter.add_points(point_cloud, cmap="viridis", point_size=1)
+    plotter.set_background("white")
+    plotter.show()
+
+
 if __name__=="__main__":
     setup_logging()
     logger = logging.getLogger(__name__)
@@ -46,4 +81,20 @@ if __name__=="__main__":
         download_lidar_ign_tiles_db(zip_output_dir)
 
     # Extract files from zip
-    extract_zip(zip_filepath, zip_output_dir)
+    # extract_zip(zip_filepath, zip_output_dir)
+
+    # download tile
+    example_url = 'https://storage.sbg.cloud.ovh.net/v1/AUTH_63234f509d6048bca3c9fd7928720ca1/ppk-lidar/BE/LHD_FXX_0188_6861_PTS_C_LAMB93_IGN69.copc.laz'
+    # wget.download(example_url, out='sandbox/data_grille')
+
+
+    # 'laz' to 'las'
+    array = laz_to_numpy('sandbox/data_grille/LHD_FXX_0188_6861_PTS_C_LAMB93_IGN69.copc.laz')
+    print(f'All data shape : {array.shape}')
+    print(f'example first data : {array[0]}')
+
+    points = array[:, :3]
+    types =  array[:, 4]
+
+    # Call the function to display the point cloud
+    show_point_cloud(points, types)
