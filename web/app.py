f0313480f4e4204d66f66fbdc51daca9bb1e4f13
ArthurDelannoyazerty <ah.delannoy@gmail.com>
2025-01-24 10:28:00 +0100

    added output filepath to flask gpkg file

--- web/app.py
+++ web/app.py
@@ -1,11 +1,15 @@
 import json
 import geopandas as gpd
+
 from shapely.geometry import shape
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO
+from datetime import datetime
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+gpkg_folder = 'data/gpkg/'
 
 @app.route('/')
 def index():
@@ -20,8 +24,9 @@ def handle_geometry(data):
         geo_df = gpd.GeoDataFrame(geometry=geometries, crs="EPSG:4326")
 
         # Save as a GeoPackage
-        output_file = "selected_zone.gpkg"
-        geo_df.to_file(output_file, driver="GPKG")
+        output_file = f'{int(datetime.now().timestamp())}--{request.sid}--gpkg_zone.gpkg'
+        output_filepath = gpkg_folder + output_file
+        geo_df.to_file(output_filepath, driver="GPKG")
         print(f"GeoPackage saved: {output_file}")
     except Exception as e:
         print(f"Error processing geometry: {e}")
