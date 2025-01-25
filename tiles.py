5e65f440e51125b577b7c58c6306cbeb050f97cb
ArthurDelannoyazerty <ah.delannoy@gmail.com>
2025-01-25 22:50:53 +0100

    updated requirements + finished pipeline

--- tiles.py
+++ tiles.py
@@ -1,24 +1,18 @@
-import sys
 import wget
 import logging
 import os
-import zipfile
 import laspy
-import numpy as np
-import pyvista as pv
-import laspy
-import numpy as np
-import open3d as o3d
-import geopandas as gpd
 import requests
 import json
+import meshlib
+import numpy as np
+import pyvista as pv
+import open3d as o3d
 import geopandas as gpd
+
 from shapely.geometry import mapping
 from shapely.ops import unary_union
-import json
-
 from shapely.vectorized import contains
-from shapely.geometry import Point
 from pathlib import Path
 from utils.logger import setup_logging
 
@@ -133,6 +127,7 @@ def decimate_array(array:np.ndarray, percentage_to_remove):
     logger.info(f'Decimation done. Number of points : {array.shape[0]} (before) | {decimated_array.shape[0]} (after)')
     return decimated_array
 
+
 def display_point_cloud(points):
     """
     Display a 3D point cloud using PyVista.
@@ -146,62 +141,6 @@ def display_point_cloud(points):
     plotter.add_points(point_cloud, cmap="viridis", point_size=1)
     plotter.set_background("white")
     plotter.show()
-
-
-
-def ply_pointcloud_to_ply_mesh_poisson(ply_pointcloud_filepath:str, 
-                                       ply_mesh_filepath:str, 
-                                       depth:int=8, 
-                                       width:float=0, 
-                                       scale:float=1.1, 
-                                       linear_fit:bool=False):
-    logger.info('Begin operation point cloud to mesh with poisson method.')
-    
-    # Load the point cloud
-    pcd = o3d.io.read_point_cloud(ply_pointcloud_filepath)
-    
-    # Estimate normals
-    pcd.estimate_normals()
-    logger.info('Estimated normals done')
-
-    # Point cloud to mesh
-    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
-        pcd,
-        depth=depth,
-        width=width,
-        scale=scale,
-        linear_fit=linear_fit
-    )[0]
-    logger.info('Poisson process done')
-
-    logger.info('Estimating mesh vertex normals')
-    mesh.compute_vertex_normals()
-    mesh.paint_uniform_color(np.array([[0.5],[0.5],[0.5]]))
-
-    # Save the mesh
-    o3d.io.write_triangle_mesh(ply_mesh_filepath, mesh)
-
-
-def xyz_to_mesh(ply_pointcloud_filepath:str, mesh_filepath:str):
-    pcd = o3d.io.read_point_cloud(ply_pointcloud_filepath)
-    xyz = np.asarray(pcd.points) 
-    mesh:pv.pointset.PolyData = pv.wrap(xyz).delaunay_2d(alpha=0.0, progress_bar=True)
-    mesh = mesh.compute_normals(progress_bar=True)
-    mesh.save(mesh_filepath)
-    return mesh
-
-
-
-def display_ply_mesh(mesh_filepath:str):
-    logger.info('Display mesh.')
-    mesh = o3d.io.read_triangle_mesh(mesh_filepath)
-
-    # Check if the mesh has vertex normals; if not, compute them
-    if not mesh.has_vertex_normals():
-        mesh.compute_vertex_normals()
-
-    # Visualize the mesh
-    o3d.visualization.draw_geometries([mesh], mesh_show_wireframe=True)
 
 
 def get_intersecting_tiles_from_order(geojson_order_filepath:str, geojson_all_tiles_available_filepath:str) -> gpd.GeoDataFrame:
@@ -238,6 +177,22 @@ def filter_points_by_polygon(xyz, polygon):
     return filtered_points
 
 
+def meshlib_terrain_point_cloud_to_mesh(ply_pointcloud_filepath:str, mesh_filepath:str):
+    logger.info('Beginning Point cloud to mesh')
+    pcd = o3d.io.read_point_cloud(ply_pointcloud_filepath)
+    xyz = np.asarray(pcd.points) 
+    xyz = xyz - xyz[0]
+    vector_3d = meshlib.mrmeshnumpy.fromNumpyArray(xyz)
+
+    logger.info('Triangulate terrain')
+    mesh = meshlib.mrmeshpy.terrainTriangulation(vector_3d)
+
+    logger.info('Remeshing')
+    meshlib.mrmeshpy.remesh(mesh, meshlib.mrmeshpy.RemeshSettings())
+    meshlib.mrmeshpy.saveMesh(mesh, mesh_filepath)
+
+
+
 if __name__=="__main__":
     setup_logging()
     logger = logging.getLogger(__name__)
@@ -246,7 +201,6 @@ if __name__=="__main__":
     FORCE_DOWNLOAD_ALL_TILES_AVAILABLE = False
     PERCENTAGE_POINT_TO_REMOVE = 0
     SHOW_CLOUDPOINT = False
-    DO_POISSON = True
     
     
     # Init folder tree if not existing
@@ -294,22 +248,6 @@ if __name__=="__main__":
         merged_xyz = np.array(merged_xyz).squeeze()
         numpy_to_ply(merged_xyz, ply_filepath)
 
-    exit(0)
-    # Point cloud to mesh (.ply -> .ply) with poisson method
-    if DO_POISSON:
-        depth = 10              # more = more detail & slower                                       -> Maximum depth of the tree that will be used for surface reconstruction. Running at depth d corresponds to solving on a grid whose resolution is no larger than 2^d x 2^d x 2^d. Note that since the reconstructor adapts the octree to the sampling density, the specified reconstruction depth is only an upper bound.
-        width = 0.0             # more = more/equal details & more outside anomalies                -> Specifies the target width of the finest level octree cells. This parameter is ignored if depth is specified
-        scale = 1.0             # more = less details & more outside anomalies & faster             -> Specifies the ratio between the diameter of the cube used for reconstruction and the diameter of the samples’ bounding cube.
-        linear_fit = True       #                                                                   -> If true, the reconstructor will use linear interpolation to estimate the positions of iso-vertices.
-
-        mesh_folder = 'data/mesh/'
-        mesh_filename = f'poisson--depth-{int(depth*100)}--width-{int(width*100)}--scale-{int(scale*100)}--linear_fit-{linear_fit}---{ply_filename}'
-        mesh_filepath = mesh_folder + mesh_filename
-
-        ply_pointcloud_to_ply_mesh_poisson(ply_filepath, 
-                                           mesh_filepath, 
-                                           depth, 
-                                           width, 
-                                           scale, 
-                                           linear_fit)
-        display_ply_mesh(mesh_filepath)
+    # Point cloud to mesh
+    mesh_filepath = order_filepath + 'mesh.stl'
+    meshlib_terrain_point_cloud_to_mesh(ply_filepath, mesh_filepath)
